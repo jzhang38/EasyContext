d01d4ad73be0d5407fb6498508dce6ff3197c131
jzhang38 <a1286225768@gmail.com>
2024-04-13 10:54:23 +0800

    delete

--- upload.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import HfApi
-api = HfApi()
-
-api.upload_folder(
-    folder_path="output/h2o_bs_1M_step_400_lr_2e-5_64K_rope_5M",
-    repo_id="PY007/EasyContext-64K-h2o",
-    repo_type="model"
-)
