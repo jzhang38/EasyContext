22948577dec7f25ef6e106de288d4db005d79819
陈坤 <76744114+Kwen-Chen@users.noreply.github.com>
2024-05-11 19:27:11 +0800

    Support ulysses flash attn (#28)
    * Support ulysses sequence parallelism
    
    * Support ulysses sequence parallelism

--- easy_context/__init__.py
+++ easy_context/__init__.py
@@ -4,6 +4,8 @@ from .zigzag_ring_attn.prepare_inputs import prepare_zigzag_ring_attn_inputs
 from .zigzag_ring_attn.monkey_patch import apply_zigzag_ring_attn_monkey_patch_llama    
 from .zigzag_ring_attn.monkey_patch import apply_zigzag_ring_attn_monkey_patch_mistral
 from .unsloth_offloaded_gradient_checkpoint.monkey_patch import apply_unsloth_offloaded_gradient_checkpoint_monkey_patch
+from .ulysses_attn.prepare_inputs import prepare_ulysses_attn_inputs  
+from .ulysses_attn.monkey_patch import apply_ulysses_attn_monkey_patch_llama 
 
 def prepare_seq_parallel_inputs(
     seq_algo, input_ids, position_ids, target_ids, rank, world_size, device
@@ -14,6 +16,10 @@ def prepare_seq_parallel_inputs(
         )
     elif seq_algo == "dist_flash_attn":
         return prepare_dist_flash_attn_inputs(
+            input_ids, position_ids, target_ids, rank, world_size, device
+        )
+    elif seq_algo == "ulysses_attn":
+        return prepare_ulysses_attn_inputs(
             input_ids, position_ids, target_ids, rank, world_size, device
         )
     elif seq_algo == "data_parallel":
@@ -28,7 +34,7 @@ def prepare_seq_parallel_inputs(
 def apply_seq_parallel_monkey_patch(
     seq_algo, model
 ):
-    assert seq_algo in ["zigzag_ring_attn", "dist_flash_attn", "data_parallel"], f"Invalid seq_algo: {seq_algo}"
+    assert seq_algo in ["zigzag_ring_attn", "dist_flash_attn", "ulysses_attn", "data_parallel"], f"Invalid seq_algo: {seq_algo}"
     assert model in ["llama", "mistral"], f"Invalid model: {model}"
     if seq_algo == "data_parallel":
         return
@@ -38,6 +44,8 @@ def apply_seq_parallel_monkey_patch(
         apply_zigzag_ring_attn_monkey_patch_mistral()
     elif seq_algo == "dist_flash_attn" and model == "llama":
         apply_dist_flash_attn_monkey_patch_llama()
+    elif seq_algo == "ulysses_attn" and model == "llama":
+        apply_ulysses_attn_monkey_patch_llama()
     else:
         raise ValueError(f"Invalid seq_algo: {seq_algo} or model: {model}")
         
